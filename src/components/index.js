3bf9a81ae55d7f758e03fa6e45a1568cb659e15a
Zhengyi Zheng <zhengzhengyi1@gmail.com>
2019-10-24 00:32:45 -0700

    added topheader, navbar and home component as well as router

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,7 @@ import * as Charts from 'react-vis';
 export * from '@material-ui/core';
 export {Icons, Charts};
 
+export {default as TopHeader} from './TopHeader';
+export {default as NavBar} from './NavBar';
+export {default as Footer} from './Footer';
+
