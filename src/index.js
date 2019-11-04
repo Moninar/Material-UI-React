36ac6d67c7085fd75e15422a641babcc20cf8a29
Zhengyi Zheng <zhengzhengyi1@gmail.com>
2019-11-03 19:34:34 -0800

    added router basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/Material-UI-React">
       <App />
     </BrowserRouter>
   </Provider>,
