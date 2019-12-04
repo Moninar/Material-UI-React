9915b5c81039e995845f5853fb888c1993e3aa9b
Zhengyi Zheng <zhengzhengyi1@gmail.com>
2019-12-04 14:48:18 -0800

    Updates

--- precache-manifest.b44ac79f7e253b2748a7d201f32e40c0.js
+++ precache-manifest.b44ac79f7e253b2748a7d201f32e40c0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a51b085f80881c58968d250236331f4d",
+    "revision": "c873493383add8162cb2cb3f1f8aa346",
     "url": "/Material-UI-React/index.html"
   },
   {
-    "revision": "cd7a3998ac2c737ac000",
+    "revision": "597d792b2b01298f683c",
     "url": "/Material-UI-React/static/css/main.8c4c956a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Material-UI-React/static/js/2.5f4e5433.chunk.js"
   },
   {
-    "revision": "cd7a3998ac2c737ac000",
-    "url": "/Material-UI-React/static/js/main.ffe870a6.chunk.js"
+    "revision": "597d792b2b01298f683c",
+    "url": "/Material-UI-React/static/js/main.0cd63a67.chunk.js"
   },
   {
     "revision": "c42f4182e22f881e814b",
